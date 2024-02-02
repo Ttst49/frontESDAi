6439253d41f20f48eb22eea35471ebecb9081e53
Ttst <thibautstachnick@gmail.com>
2024-02-02 20:22:55 +0100

    add app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {PromptZoneComponent} from "./prompt-zone/prompt-zone.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PromptZoneComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
