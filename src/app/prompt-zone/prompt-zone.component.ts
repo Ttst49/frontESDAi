e1907ee156edced7c234400edc32899c0e597efb
Ttst <thibautstachnick@gmail.com>
2024-02-04 16:57:02 +0100

    add logic for ia call

--- src/app/prompt-zone/prompt-zone.component.ts
+++ src/app/prompt-zone/prompt-zone.component.ts
@@ -1,12 +1,14 @@
 import {Component, inject} from '@angular/core';
 import {PromptService} from "../prompt.service";
 import {FormsModule} from "@angular/forms";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-prompt-zone',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    NgIf
   ],
   templateUrl: './prompt-zone.component.html',
   styleUrl: './prompt-zone.component.css'
@@ -16,14 +18,14 @@ export class PromptZoneComponent {
 
   promptService:PromptService = inject(PromptService)
   prompt!:string
-  response!:string
+  response:string = "Envoyez un prompt d'abord"
 
   sendPrompt(prompt:string){
     this.promptService.sendAPrompt(prompt).subscribe({
       next:(responseFromPrompt:any)=>{
-        this.response = responseFromPrompt
+        this.response = responseFromPrompt.response
+        console.log(responseFromPrompt)
       }
     })
   }
-
 }
